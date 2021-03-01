e7d5f192efda6f2ae055d274ffb86baabf8d85e8
Hexzel <a0972180845@gmail.com>
2021-02-28 22:56:04 -0800

    add main.cpp

---
+++ mbed-os-test-program/main.cpp
@@ -0,0 +1,16 @@
+#include "mbed.h"
+
+DigitalOut myled(LED2);
+DigitalOut myled2(LED1);
+
+int main() {
+    while (1) {
+        myled = 1;
+        myled2 = 0;
+        ThisThread::sleep_for(1s);
+
+        myled.write(0);
+        myled2.write(1);
+        ThisThread::sleep_for(1s);
+    }
+}
